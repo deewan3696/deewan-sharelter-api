78198f53d1c418215ddbb4d8152dbeb285f29295
deewan3696 <mail_d1@yahoo.com>
2023-11-06 10:29:59 +0100

    listing created

--- src/routes/listing.js
+++ src/routes/listing.js
@@ -16,6 +16,44 @@ const {
 } = require("../controllers/listing");
 
 
+
+
+/**
+ * help create lisitng for a customer
+ * @swagger
+ * /listings:
+ *   post:
+ *     summary: creates a new listing
+ *     description: This Creates a new listing for the user
+ *     tags:
+ *       - Listing
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: number_of_listing
+ *         in: body
+ *         required: true
+ *       - name: location
+ *         in: body
+ *         required: true
+ *       - name: photo_url
+ *         in: body
+ *         required: true
+ *       - name: price
+ *         in: body
+ *         required: true
+ *       - name: listing_types
+ *         in: body
+ *         required: true
+ *       - name: occupancy_limit
+ *         in: body
+ *         required: true
+ *     responses:
+ *        201:
+ *          description: Listing created.
+ *        422:
+ *          Bad Request
+ */
 router.post(
   "/create-listing",
   authorization,
@@ -23,7 +61,42 @@ router.post(
   startListing
 );
 
-
+/**
+ * help to make payment for  listing for a customer
+ * @swagger
+ * /listings:
+ *   post:
+ *     summary: completely creates a new listing
+ *     description: This Creates a new listing for the user
+ *     tags:
+ *       - Listing
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: payment_reference
+ *         in: body
+ *         required: true
+ *       - name: amount
+ *         in: body
+ *         required: true
+ *       - name: 
+ *         in: body
+ *         required: 
+ *       - name: 
+ *         in: body
+ *         required: 
+ *       - name: 
+ *         in: body
+ *         required: 
+ *       - name: 
+ *         in: body
+ *         required: 
+ *     responses:
+ *        201:
+ *          description: Listing created with payment confirmation.
+ *        422:
+ *          Bad Request
+ */
 
 router.post(
   "/complete-listing",
